207d5caf0d9766cc8f2914e7cf3c73bdad4e33aa
GinaDan1 <georginadanquah1@gmail.com>
2023-02-18 01:40:40 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Program that prints a newline with puts function
+ * Return: 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
